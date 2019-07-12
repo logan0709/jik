3db6c29ace0ca783e03ba2d9a30ee2a169175191
刘太全 <646154945@qq.com>
2019-07-12 12:33:21 +0800

    jik

--- md2html/md2html.py
+++ md2html/md2html.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # 使用方法 python md2html.py filename
+# pip install markdown
 
 import sys
 import markdown
@@ -8,12 +9,13 @@ import codecs
 
 
 css = '''
+<html>
+<head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<link rel="stylesheet" href="https://guides.github.com/components/gridism/gridism.css">
 <link href="markdown.css" rel="stylesheet">
-<link rel="stylesheet" href="https://guides.github.com/components/primer/octicons.css">
-<link href="https://guides.github.com/stylesheets/main.css" rel="stylesheet">
-<link href="https://guides.github.com/stylesheets/pygments.css" rel="stylesheet">
+</head>
+<body>
+<article class="markdown-body">
 '''
 
 def main(argv):
@@ -23,23 +25,22 @@ def main(argv):
 
     input_file = codecs.open(in_file, mode="r", encoding="utf-8")
     text = input_file.read()
-    ext = [ 'markdown.extensions.toc',\
-            'markdown.extensions.sane_lists',\
-            'markdown.extensions.codehilite',\
-            'markdown.extensions.abbr',\
-            'markdown.extensions.attr_list',\
-            'markdown.extensions.def_list',\
-            'markdown.extensions.fenced_code',\
-            'markdown.extensions.footnotes',\
-            'markdown.extensions.smart_strong',\
-            'markdown.extensions.meta',\
-            'markdown.extensions.nl2br',\
-            'markdown.extensions.tables']
+    ext = ['markdown.extensions.toc',
+           'markdown.extensions.codehilite',
+           'markdown.extensions.sane_lists',
+           'markdown.extensions.abbr',
+           'markdown.extensions.attr_list',
+           'markdown.extensions.def_list',
+           'markdown.extensions.fenced_code',
+           'markdown.extensions.footnotes',
+           'markdown.extensions.meta',
+           'markdown.extensions.nl2br',
+           'markdown.extensions.tables']
 
     html = markdown.markdown(text, output_format='html5', extensions=ext)
 
     output_file = codecs.open(out_file, "w",encoding="utf-8",errors="xmlcharrefreplace")
-    output_file.write(css+html)
+    output_file.write(css+html+"</article></body>")
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
