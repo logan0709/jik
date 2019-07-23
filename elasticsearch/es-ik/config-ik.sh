f28b4c0302edbba6832e44cf5239a40e081a0033
刘太全 <646154945@qq.com>
2019-07-23 09:11:36 +0800

    es

--- elasticsearch/es-ik/config-ik.sh
+++ elasticsearch/es-ik/config-ik.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
-cat >> plugins/ik/config/IKAnalyzer.cfg.xml <<EOF \
+cat >> plugins/ik/config/IKAnalyzer.cfg.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
 <properties>
