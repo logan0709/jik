2ba07849ec7f8a1e89a3d03ac4d6934cbcf64530
刘太全 <646154945@qq.com>
2019-07-25 15:08:18 +0800

    es

---
+++ elasticsearch/es-ik/docker-entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#cluster.name='elasticsearch-cluster'
+#discovery.zen.ping.unicast.hosts='elasticsearch-discovery'
+#node.master='true'
+#node.data='true'
+es_opts=''
+while IFS='=' read -r envvar_key envvar_value
+do
+  if [[ "$envvar_key" =~ ^[a-z0-9_]+\.[a-z0-9_]+ || "$envvar_key" == "processors" ]]; then
+    if [[ ! -z $envvar_value ]]; then
+      es_opt="-E${envvar_key}=${envvar_value}"
+      es_opts+="${es_opt}"
+    fi
+  fi
+done < <(env)
+echo $es_opts
